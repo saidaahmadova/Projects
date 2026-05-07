ecdbc0d6625aa3a23703e1b1219dd6ec829195e6
saidaahmadova <saidaava7@gmail.com>
2026-05-07 22:47:35 +0400

    Saving Calculator

---
+++ Saving Calculator.py
@@ -0,0 +1,7 @@
+print("Welcome to the saving calculator")
+week = int(input("How much do you save per week? $"))
+how = int(input("How many week?"))
+total = week * how
+
+print(f"You will save ${total} in {week}")
+
