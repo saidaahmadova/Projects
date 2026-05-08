7c0967b9d00c9b1b9d975dc099ddc76c1c5a2fb7
saidaahmadova <saidaava7@gmail.com>
2026-05-09 00:22:42 +0400

    Treasure Island Project 2

--- task.py
+++ task.py
@@ -1,20 +1,24 @@
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+choice1 = input('You\'re at a cross road. Where do you want to go? '
+                'Type "left" or "right"').lower()
 
-print("Welcome to the pizaa calculator")
-size = input("What size do you have? S, M, L?")
-toppings = int(input("How many toppings do you have?"))
-delivery = input("Do you want delivery? Yes or No?")
+if choice1 == "left":
+    choice2 = input('You\'ve come to a lake. '
+                    'There is an island in the middle of the lake.'
+                    'Type "wait" to wait for a boat. Type "swim" to swim across.').lower()
+    if choice2 == "wait":
+        choice3 = input("You arrived at the island unharmed. "
+              "There is a house with 3 doors. One red, one yellow and one blue. "
+              "Which colour do you choose?").lower()
+        if choice3 == "red":
+            print("It's a room full of fore. Game Over")
+        elif choice3 == "yellow":
+            print("You found the treasure! You win!")
+        elif choice3 == "blue":
+            print("It's a room full of fore. Game Over")
+        else:
+            print("You chose a door that doesn't exist.Game Over")
+    else:
+        print("You fell in to a hole. Game Over.")
 
-bill = 0
-if size == "S":
-    bill = 10
-elif size == "M":
-    bill = 15
-elif size == "L":
-    bill = 20
-
-bill += toppings * 2
-
-if delivery == "Yes":
-    bill += 5
-
-print(f"Your final bill is ${bill}")
